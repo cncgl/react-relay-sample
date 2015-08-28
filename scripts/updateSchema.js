0a3d476c81987c0aa2fcee96c91abcc8366ac589
Shigeru Kondou <concigel@gmail.com>
2015-08-28 12:05:06 +0900

    update eslint setting

--- scripts/updateSchema.js
+++ scripts/updateSchema.js
@@ -8,19 +8,19 @@ import { introspectionQuery, printSchema } from 'graphql/utilities';
 
 // Save JSON of full schema introspection for Babel Relay Plugin to use
 async () => {
-  var result = await (graphql(Schema, introspectionQuery));
-  if (result.errors) {
-    console.error(
-      'ERROR introspecting schema: ',
-      JSON.stringify(result.errors, null, 2)
-    );
-  } else {
-    fs.writeFileSync(
-      path.join(__dirname, '../data/schema.json'),
-      JSON.stringify(result, null, 2)
-    );
-  }
-}();
+    var result = await (graphql(Schema, introspectionQuery));
+    if (result.errors) {
+        console.error(
+            'ERROR introspecting schema: ',
+            JSON.stringify(result.errors, null, 2)
+        );
+    } else {
+        fs.writeFileSync(
+            path.join(__dirname, '../data/schema.json'),
+            JSON.stringify(result, null, 2)
+        );
+    }
+};
 
 // Save user readable type system shorthand of schema
 fs.writeFileSync(
